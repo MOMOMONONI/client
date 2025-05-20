f93b44d22dba93cf9a55b44a1d6388e0f0cfd3f0
Noar2000 <6403011011@feu.edu>
2025-05-20 23:47:24 +0700

    เส้นทางผิด

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -42,7 +42,7 @@ const Register = () => {
 
   const onSubmit = async (data) => {
     try {
-      const res = await axios.post("https://api-omega-seven-66.vercel.app/api/admin/orders", data);
+      await axios.post("https://api-omega-seven-66.vercel.app/api/register", data);
       toast.success("สมัครสมาชิกสำเร็จ");
     } catch (err) {
       const errMsg = err.response?.data?.message;
